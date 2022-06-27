e9ea0498872a4bf89d3b0359d1ebb580f9720129
Cesar Brandon <cesarbrandon000@gmail.com>
2022-06-26 20:52:07 -0500

    no-cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 //Settings
 
 //Middlewares
-const whiteList = ["https://demo-notepad-react.vercel.app/"];
+const whiteList = ["https://demo-notepad-react.vercel.app"];
 app.use(cors({ origin: whiteList }));
 app.use(express.json());
 
