a6eceab6db97fa93a894c9f7f495266821510b5d
Cesar Brandon <cesarbrandon000@gmail.com>
2022-06-26 19:25:55 -0500

    deploy api

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const dbConnect = () => {
-  const DB_URI = process.env.DB_URI;
   mongoose.connect(
-    DB_URI,
+    process.env.DB_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
